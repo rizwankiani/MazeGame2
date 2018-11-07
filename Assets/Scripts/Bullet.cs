94a42b6a1cee3f0259c8f85594477cd10f7e3f43
rizwankiani <rizwan@tlu.ee>
2018-11-07 19:59:10 +0200

    Player Action

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-
+    //Members
+	public float moveSpeed = 7.0f;
+	
+	private float timePassed = 0.0f;
 	// Use this for initialization
 	void Start () {
 		
 	}
+	// functions
+	
 	
 	// Update is called once per frame
 	void Update () {
+		transform.Translate(Vector3.forward * moveSpeed) * Time.deltaTime);
+		timePassed += 1 * Time.deltaTime;
+if (timePassed >= 5)
+	{
+		Destroy(this.gameObject);
 		
+	}
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     // members or variables
     public float moveSpeed = 7.0f;
+	public GameObject bulletSpawner;
+	public GameObject bullet;
 
     // methods or functions
 	
@@ -14,6 +16,29 @@ public class Player : MonoBehaviour {
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+			//transform.GetChild(0).LockAt();
         }
+		 if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+		 if(Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+		 if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+		if(Input.GetKeyDown(KeyCode.Space)){
+			
+			Shoot();
+			
+			}}
+			
+		void Shoot(){
+			Instantiate(bullet.transform,bulletSpawner.transform.position, bulletSpawner.transform.rotation);
+			
+			}
+		
 	}
-}
